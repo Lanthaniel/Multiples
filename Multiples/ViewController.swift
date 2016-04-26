a3a7276919b32cafc3ff622d4462e224e0c60889
Nate <natejoe@gmail.com>
2016-04-25 22:57:11 -0400

    Completion of code and demo

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,6 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Properties
+    var multiple: Int = 0
+    var sum: Int = 0
+    var counter: Int = 0
+    
+    //Main Menu Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var multiplesText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    //Game Screen Outlets
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    @IBAction func onPlayBtnPress(sender: UIButton!) {
+        
+        if (multiplesText.text != nil && multiplesText.text != "") {
+            
+            //Hide the main menu
+            logoImg.hidden = true
+            multiplesText.hidden = true
+            playButton.hidden = true
+            
+            //Reveal the Game Screen
+            gameLabel.hidden = false
+            addButton.hidden = false
+            gameLabel.text = "Press Add to add!"
+            
+            //Set up addition
+            multiple = Int(multiplesText.text!)!
+            sum = 0
+            counter = 0
+        }
+    }
+    
+    @IBAction func onAddBtnPress(sender: UIButton!) {
+        
+        //Only due multiples up to 12
+        if (counter < 12) {
+            gameLabel.text = "\(sum) + \(multiple) = \(sum+multiple)"
+            sum += multiple
+            counter += 1
+        } else {
+            gameIsOver()
+        }
+        
+    }
+    
+    
+    func gameIsOver() {
+        
+        //Hide the Game Screen
+        gameLabel.hidden = true
+        addButton.hidden = true
+        
+        //Reveal the main menu
+        logoImg.hidden = false
+        multiplesText.hidden = false
+        playButton.hidden = false
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
